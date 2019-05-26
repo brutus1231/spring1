470ebefcf4afbb10448b0a765ae304ede9424af1
brutus1231 <ztrzaska2@gmail.com>
2019-05-26 10:14:25 +0200

    zad4

--- src/main/java/pl/sda/Zad4.java
+++ src/main/java/pl/sda/Zad4.java
@@ -3,13 +3,22 @@ package pl.sda;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Zad4 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("my-beans.xml");
 
-        List<String> napisy = (List)context.getBean("mojaLista");
+        List<String> napisy = (List) context.getBean("mojaLista");
         napisy.stream().forEach(System.out::println);
+
+        Map<String, String> myMap = (Map<String, String>) context.getBean("mojaMapa");
+
+        Set<String> keys = myMap.keySet();
+        for (String key : keys) {
+            System.out.println(key + " " + myMap.get(key));
+        }
     }
 }
